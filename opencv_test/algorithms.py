98d34813683429aee297680abc639b37716be176
taco650 <taco650@gmail.com>
2019-11-22 18:37:41 -0800

    Added open source material

--- opencv_test/algorithms.py
+++ opencv_test/algorithms.py
@@ -1,10 +1,17 @@
 import cv2
 import numpy as np
 import os
+import sys
+import math
+import os
+import logging
+import geom_util as geom
+import roi
+import track_conf as tconf
 
 # Set whether images should be displayed/saved
-show = True
-write = False
+show = False
+write = True
 
 def main():
     # Images
@@ -19,10 +26,11 @@ def main():
     for f in files:
         img = cv2.imread('images/{}.jpg'.format(f), cv2.IMREAD_GRAYSCALE)
 
-        showOriginal(img, f)
-        showProcessed(img, f)
+        threshold(img, f)
+        a_method(img, f)
+        contours(img, f)
 
-def showOriginal(img, f):
+def threshold(img, f):
     # Show original image
     # cv2.imshow('Original Image', img)
 
@@ -34,9 +42,9 @@ def showOriginal(img, f):
 
     # Save to file
     if write:
-        cv2.imwrite('output/{}_A.jpg'.format(f), img)
+        cv2.imwrite('output/{}_Threshold.jpg'.format(f), img)
 
-def showProcessed(img, f):
+def a_method(img, f):
     # Image processing
 
     # Threshold image first
@@ -92,7 +100,61 @@ def showProcessed(img, f):
 
     # Save to file
     if write:
-        cv2.imwrite('output/{}_B.jpg'.format(f), img)
+        cv2.imwrite('output/{}_A.jpg'.format(f), img)
+
+
+
+def contours(img, f):
+    # Threshold image first
+    thresh = 200
+    _, img = cv2.threshold(img, thresh, 255, cv2.THRESH_BINARY)
+
+    # nxn mean filter
+    n = 9
+    mean_filter = np.ones((n, n), np.float32) / (n*n)
+    # img = cv2.filter2D(img, -1, mean_filter)
+
+    # nxn median filter
+    n = 9
+    img = cv2.medianBlur(img, n)
+
+    # nxn bilateral filter
+    # n = 5
+    # s_sigma = 100 # spatial sigma
+    # i_sigma = 100 # intensity sigma
+    # img = cv2.bilateralFilter(img, n, s_sigma, i_sigma)
+
+    # nxn Scharr filter
+    n = -1
+    scharr_x = np.uint8(np.absolute(cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=n)))
+    scharr_y = np.uint8(np.absolute(cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=n)))
+    img = cv2.addWeighted(scharr_x, 0.5, scharr_y, 0.5, 0)
+
+    # img = cv2.bitwise_not(img)
+
+    # Test morphology
+
+    # Dilation and erosion w/ n iterations
+    n = 6
+    img = cv2.dilate(img, mean_filter, iterations=n)
+    img = cv2.erode(img, mean_filter, iterations=n)
+
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    color_img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+
+    cv2.drawContours(color_img, contours, -1, (0,255,0), 3)
+    
+    if show:
+        cv2.imshow('Processed Image Contours', color_img)
+        cv2.waitKey(0)
+
+    # Save to file
+    if write:
+        cv2.imwrite('output/{}_Contours.jpg'.format(f), color_img)
+
+
+
 
 if __name__ == '__main__':
     main()
