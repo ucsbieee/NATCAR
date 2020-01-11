474d989683e10d6b40bf0ca4af951b425da26617
sansangela <sansangela@hotmail.com>
2020-01-10 19:49:24 -0800

    update

---
+++ pi_cam/timelapse.py
@@ -0,0 +1,18 @@
+from picamera import PiCamera
+import os
+
+takePhoto = True
+
+def main():
+
+    os.chdir('/home/pi/img')
+
+    camera = PiCamera()
+    camera.resolution = (640, 480)
+    i = 0
+
+    while takePhoto: 
+        camera.capture('image{0:04d}.jpg'.format(i))
+        i += 1
+    
+    camera.close()
