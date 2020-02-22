f8c64dd5ad4c8872010e515b0d6c4fde27b46ce4
sansangela <sansangela@hotmail.com>
2020-02-21 21:27:21 -0800

    save img

--- opencv_test/timelapse.py
+++ opencv_test/timelapse.py
@@ -1,21 +1,26 @@
 # import os
 import io
+import os
 import time
 import picamera
 import threading
+import cv2
 from PIL import Image
 from time import sleep
 
 stream = io.BytesIO()
-with picamera.PiCamera() as camera:
-    camera.resolution = (640, 480)
-    camera.start_preview()
-    time.sleep(2)
+camera = picamera.PiCamera()
+# with picamera.PiCamera() as camera:
+camera.resolution = (640, 480)
+camera.start_preview()
+time.sleep(2)
 # takePhoto = True
-lock = Lock()
+lock = threading.Lock()
 
 def thread_function():
     while True:
+        global camera
+        global stream
         lock.acquire()
         camera.capture(stream, format='jpeg')
         lock.release()
@@ -23,10 +28,15 @@ def thread_function():
         stream.truncate()
 
 def thread_function2():
+    global camera
+    global stream
     lock.acquire()
     stream.seek(2)
     image = Image.open(stream)
-    image.show()
+
+    os.chdir('/home/pi/Desktop')
+    cv2.imwrite('data.jpeg',image)
+    # image.show()
     lock.release()
 
 def main():
@@ -38,7 +48,11 @@ def main():
     y = threading.Thread(target=thread_function2)
     x.start()
     y.start()
-    y.join()
+    for i in range(10):
+        print("new thread started!")
+        y = threading.Thread(target=thread_function2)
+        y.start()
+        y.join()
     
     # "Rewind" the stream to the beginning so we can read its content
 
