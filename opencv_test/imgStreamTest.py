083c6df033105905164fecc4653820bf4389bf75
jsun454 <jsun46@umail.ucsb.edu>
2020-02-21 20:46:53 -0800

    test capture instead of capture continuous

--- opencv_test/imgStreamTest.py
+++ opencv_test/imgStreamTest.py
@@ -9,18 +9,34 @@ stream = None
 def initStream():
     global stream
 
+    # stream = io.BytesIO()
+    # with PiCamera() as camera:
+    #     camera.resolution = (640, 480)
+    #     camera.capture_continuous(stream, 'jpeg', use_video_port=True)
+
+    # while True:
+    #     try:
+    #         stream.seek(0)
+    #         data = np.fromstring(stream.getvalue(), dtype=np.uint8)
+    #         image = cv2.imdecode(data, 1)[:, :, ::-1]
+    #         cv2.imshow('Image', image)
+    #         stream.truncate()
+    #     except:
+    #         continue
+
     stream = io.BytesIO()
+
     with PiCamera() as camera:
         camera.resolution = (640, 480)
-        camera.capture_continuous(stream, 'jpeg', use_video_port=True)
-
-    while True:
+        camera.start_preview()
+        time.sleep(2)
+        while True:
+            camera.capture(stream, format='jpeg')
+            # camera.capture_continuous(stream, 'jpeg', use_video_port=True)
         try:
-            stream.seek(0)
             data = np.fromstring(stream.getvalue(), dtype=np.uint8)
             image = cv2.imdecode(data, 1)[:, :, ::-1]
             cv2.imshow('Image', image)
-            stream.truncate()
         except:
             continue
 
