9f8ea25c07aeebc9beab6cc3ec47934b262f5a6e
sansangela <sansangela@hotmail.com>
2020-02-21 20:09:21 -0800

    updated thread

--- opencv_test/timelapse.py
+++ opencv_test/timelapse.py
@@ -12,11 +12,7 @@ with picamera.PiCamera() as camera:
     camera.start_preview()
     time.sleep(2)
 # takePhoto = True
-
-def thread_function():
-    while True:
-        camera.capture(stream, format='jpeg')
-
+    
 def thread_function2():
     image = Image.open(stream)
     image.show()
@@ -26,10 +22,13 @@ def main():
     # os.chdir('/home/pi/Desktop/images')
 
     # Create the in-memory stream
-    x = threading.Thread(target=thread_function)
+    # x = threading.Thread(target=thread_function)
+    while True:
+        camera.capture(stream, format='jpeg')
     y = threading.Thread(target=thread_function2)
-    x.start()
+    # x.start()
     y.start()
+    y.join()
 
     # "Rewind" the stream to the beginning so we can read its content
 
