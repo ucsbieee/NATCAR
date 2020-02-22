35daffe3a4c1ad42780248d69db3de6f64ec4367
neenbeannnn <hmn.nini1970@gmail.com>
2020-02-21 21:13:33 -0800

    added in global vars

--- opencv_test/timelapse3.py
+++ opencv_test/timelapse3.py
@@ -12,10 +12,12 @@ with picamera.PiCamera() as camera:
     camera.start_preview()
     time.sleep(2)
 # takePhoto = True
-lock = Lock()
+lock = threading.Lock()
 
 def thread_function():
     while True:
+        global camera
+        global stream
         lock.acquire()
         camera.capture(stream, format='jpeg')
         lock.release()
@@ -23,6 +25,8 @@ def thread_function():
         stream.truncate()
 
 def thread_function2():
+    global camera
+    global stream
     lock.acquire()
     stream.seek(2)
     image = Image.open(stream)
@@ -38,7 +42,11 @@ def main():
     y = threading.Thread(target=thread_function2)
     x.start()
     y.start()
-    y.join()
+    for i in range(10):
+        print("new thread started!")
+        y = threading.Thread(target=thread_function2)
+        y.start()
+        y.join()
     
     # "Rewind" the stream to the beginning so we can read its content
 
