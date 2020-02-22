5193713d8c106f915bc1d98ae205973ebfc67cc8
jsun454 <jsun46@umail.ucsb.edu>
2020-02-21 20:52:00 -0800

    test doc code

--- opencv_test/imgStreamTest.py
+++ opencv_test/imgStreamTest.py
@@ -25,20 +25,28 @@ def initStream():
     #         continue
 
     stream = io.BytesIO()
-
     with PiCamera() as camera:
-        camera.resolution = (640, 480)
         camera.start_preview()
         time.sleep(2)
-        while True:
-            camera.capture(stream, format='jpeg')
-            # camera.capture_continuous(stream, 'jpeg', use_video_port=True)
-        try:
-            data = np.fromstring(stream.getvalue(), dtype=np.uint8)
-            image = cv2.imdecode(data, 1)[:, :, ::-1]
-            cv2.imshow('Image', image)
-        except:
-            continue
+        camera.capture(stream, format='jpeg')
+    data = np.fromstring(stream.getvalue(),, dtype=np.uint8)
+    image = cv2.imdecode(data, 1)
+    image = image[:, :, ::-1]
+    cv2.imshow('Image', image)
+
+    # with PiCamera() as camera:
+    #     camera.resolution = (640, 480)
+    #     camera.start_preview()
+    #     time.sleep(2)
+    #     while True:
+    #         camera.capture(stream, format='jpeg')
+    #         # camera.capture_continuous(stream, 'jpeg', use_video_port=True)
+    #     try:
+    #         data = np.fromstring(stream.getvalue(), dtype=np.uint8)
+    #         image = cv2.imdecode(data, 1)[:, :, ::-1]
+    #         cv2.imshow('Image', image)
+    #     except:
+    #         continue
 
 def closeStream():
     global stream
