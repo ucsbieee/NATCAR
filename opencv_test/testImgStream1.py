180e5ec1411afc8fe5cdd197c2944fabc2e7a166
Ray Chang <raychang@ucsb.edu>
2020-02-28 19:58:08 -0800

    show image from stream

--- opencv_test/testImgStream1.py
+++ opencv_test/testImgStream1.py
@@ -7,25 +7,25 @@ import cv2
 import PIL
 from PIL import Image
 from time import sleep
+import numpy as np
 
 def main():
-    os.chdir('/home/pi/Desktop')
-    
-    my_file = open('my_image.jpg', 'wb')
     stream = io.BytesIO()
     camera = picamera.PiCamera()
     camera.resolution = (640, 480)
-    camera.start_preview()
     time.sleep(2)
 
-    camera.capture(my_file)
-    camera.stop_preview()
+    camera.capture(stream,format='jpeg')
     
     stream.seek(0)
     
-    my_file.close()
+    
+    data = np.frombuffer(stream.getvalue(), dtype=np.uint8)
+    img = cv2.imdecode(data,1)
+    cv2.imwrite('test.jpg', img)
+    # cv2.imshow('img', img)
+    cv2.imshow('img', cv2.imread('test.jpg'))
+    cv2.waitKey(0)
 
-    image = 'my_image.jpg'
-    cv2.imshow(image,0)
     
 main()
